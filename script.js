8992ca698802a3430381efff445757717fb4f30c
Andrey <136622037+and96437345@users.noreply.github.com>
2023-11-01 20:21:53 +0300

    Examen JS

--- script.js
+++ script.js
@@ -1,149 +1,67 @@
-// // Задание 1
-// let n = 4;  
-  
-// function factorial(n) {  
-//     let ans = 1;  
-      
-//     if(n === 0) 
-//         return 1; 
-//     for (let i = 2; i <= n; i++)  
-//         ans = ans * i;  
-//     return ans;  
-// } 
-// console.log(factorial(n));
+// 1. Создайте кнопку, при нажатии на которую будет меняться текст внутри элемента <div>.
+let textBtn = document.querySelector('.text-remake');
+textBtn.addEventListener('click', () => {
+    textBtn.innerHTML = 'Нажато';
+});
 
-// // Задание 2
-// function isPrime(num) {
-//     for (let i = 2; i < num; i++) {
-//       if (num % i === 0) return false;
-//     }
-//     return num !== 1;
-//   }
-// let array = [];
-//   function printPrimes(max) {
-//     for (let i = 2; i <= max; i++) {
-//       if (isPrime(i)) {
-//         array.push(i);
-//       };
-//     }
-//     console.log(array)
-//   }
-//   printPrimes(20);
+// 2. Напишите скрипт, который добавляет новый элемент (например <li>) в список <ul> при клике на кнопку
+let btnCreate = document.querySelector('.create')
+let parent = document.querySelector('.table');
+btnCreate.addEventListener('click', () => {
+    let element = document.createElement('li');
+    parent.append(element);
+})
 
-//   // Задание 3
-
-//   let str = "asloirasdjhfgkjas"; //строка
-//   let vowels = "aeiouy"; //гласные
-//   let pos, res = "";
-//   let i = 0;
-//   while (i < str.length) {
-//     res += str[i];
-//     if(vowels.indexOf(str[i]) > -1) {
-//       i += 3;
-//     } else {
-//       i += 2;
-//     }
-//   }
-//   console.log(res);
-
-// Задание 1
+// 3. Реализуйте функцию, которая скрывает/показывает текстовый блок при клике на кнопку Показать/Скрыть.
 
 
-// function sum(a, b) {
-//     let a = +prompt('Введите первое число','');
-//     let b = +prompt('Введите второе число','');
-//     let sum = a + b;
-//     alert(`Сумма ${sum}`);
-// }
-// sum(a,b);
+const btn = document.querySelector(".btn");
+const content = document.querySelector(".content");
 
-// Задание 2
+btn.addEventListener("click", btnClick);
 
-// function even(num) {
-//     let res = num%2;
-//     if (res == 0) {
-//         return true;
-//     } else return false;
-// }
-// alert(even(9));
+function btnClick() {
+    console.log(content.classList);
 
-// Задание 3
+    if (content.classList.contains("hidden")) {
+        btn.textContent = "Скрыть элемент";
+    } else {
+        btn.textContent = "Показать элемент";
+    }
 
-// let array = [3, -13, 56, 34, 98, -45]
-// function max(array) {
-//     let maxNum = array[0];
-//     array.forEach(i => {
-//         if (i > maxNum){
-//             maxNum = i;
-//         }
-//     });
-//     alert(maxNum);
-// }
-// max(array);
+    content.classList.toggle("hidden");
+}
 
-// Задание 4
+// 6. Реализуйте функцию, которая принимает массив чисел и возвращает новый массив, в котором все отрицательные числа заменены на 0.
 
-// let str = 'flasdikfhlksjdhfikusyd'
-// function strLength(str) {
-//     let length = str.length;
-//     alert(length);
-// }
-// strLength(str);
+let arrayNum = [1, -34, 67, 76, -45, -23];
+let arrayNew = [];
+function arrayZero(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            arrayNew.push(0);
+        } else {
+            arrayNew.push(array[i]);
+        }
+    }
+    console.log(arrayNew)
+}
+arrayZero(arrayNum);
 
-// Задание 5
+// 10. Создайте массив чисел и найдите максимальное и минимальное значение в этом массиве без использования встроенных функций
 
-// function random(dia) {
-//     let ran = Math.random() * dia;
-//     ran = Math.round(ran);
-//     alert(ran);
-// }
-// random(100);
-
-// Задание 6
-
-// function tab(num){
-//     for (let i = 1; i<10; i ++ ) {
-//         pro = i*num;
-//         console.log(`${i}*${num}=${pro}`)
-//     }
-// }
-// tab(4);
-
-// Задание 7
-
-// function hello(name) {
-//     alert('Hello '+ name)
-// }
-// hello('Иван');
-
-// Задание 9
-
-
-// function counterVowels(line){
-//     line = line.toLowerCase();
-//     counter = 0;
-//     for(let i = 0; i<line.length; i++){
-//         let char = line[i];
-//         if (char == 'a' ||  char == 'e' || char == 'i' || char == 'o' || char == 'u'){
-//             counter++;
-//         } 
-//     }
-//     console.log(counter)
-// }
-// counterVowels('sdhfglsikglaeeeeeksgleeeekjsdh')
-
-
-// function strReverse(str){
-//     str = str.split('').reverse().join('');
-//     console.log(str)
-// }
-// strReverse('Привет');
-
-
-
-
-
-
+let arrayNumber = [254, 45, -35, 6, -2, -10, 76];
+function maxMin(arr){
+    let min = arr[0];
+    let max = arr[0];
+    for (let i=0 ; i<arr.length; i++) {
+        if (arr[i]<min) min = arr[i];
+        if (arr[i]>max) max = arr[i];
+    }
+    console.log(min);
+    console.log(max);
+}
+maxMin(arrayNumber);
 
 
 
